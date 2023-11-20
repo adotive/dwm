d58136c75220d420b3cb68a93d83b549eeed3239
Gabriel Pereira <140173310+adotive@users.noreply.github.com>
2023-11-20 18:48:13 -0300

    Update config.def.h

--- config.def.h
+++ config.def.h
@@ -1,9 +1,5 @@
 /* See LICENSE file for copyright and license details. */
 
-#define TERMINAL "st"
-#define TERMCLASS "St"
-#define BROWSER "firefox"
-
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
